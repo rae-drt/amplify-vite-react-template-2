293733cb2c5ae1a16c83a6d239fbe6f0dbad3fb7
mark-bell-tna <mark.bell@nationalarchives.gov.uk>
2025-06-24 12:20:12 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
+import '@aws-amplify/ui-react/styles.css';
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+
   <React.StrictMode>
-    <App />
+    <Authenticator hideSignUp>
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
